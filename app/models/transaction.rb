0a675ed60402e399219abfc4413b1a8ad4613c99
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-09 17:51:59 -0600

    Adding validations and associations to models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
+  validates :author_id, :name, :amount, presence: true
+  validates :amount, scale: 2
+
   belongs_to :user
+  has_and_belongs_to_many :groups
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+
+  has_many :transactions
+  has_many :groups
 end
