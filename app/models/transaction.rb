dc232971219257612d774db118e43eca6898775b
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-09 17:55:22 -0600

    Fixing author association to user model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,6 @@ class Transaction < ApplicationRecord
   validates :author_id, :name, :amount, presence: true
   validates :amount, scale: 2
 
-  belongs_to :user
+  belongs_to :author, class_name: :User, foreign_key: :author_id
   has_and_belongs_to_many :groups
 end
