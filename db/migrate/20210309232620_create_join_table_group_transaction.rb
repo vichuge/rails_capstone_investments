4f610bfeb473808f77608ba51788d2df28e98ba7
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-09 17:29:06 -0600

    Add groups_transactions table on migrations and shcema

---
+++ db/migrate/20210309232620_create_join_table_group_transaction.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableGroupTransaction < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :groups, :transactions do |t|
+      # t.index [:group_id, :transaction_id]
+      # t.index [:transaction_id, :group_id]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_225458) do
+ActiveRecord::Schema.define(version: 2021_03_09_232620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2021_03_09_225458) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "groups_transactions", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "transaction_id", null: false
   end
 
   create_table "transactions", force: :cascade do |t|
