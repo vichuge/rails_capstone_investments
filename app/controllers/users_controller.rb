738e94c9950bed97d08a12df457135f5289f4606
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-09 18:42:29 -0600

    Adding users controller and views, routes too

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+      
+  end
+  
+  def show
+      
+  end
+
+  def new
+      
+  end
+
+  def create
+      
+  end
+
+  def edit
+      
+  end
+  
+  def update
+      
+  end
+end
