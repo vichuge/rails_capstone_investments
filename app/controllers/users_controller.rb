55c1cd10c8717efccf68f3bd3237e7844b82466e
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-10 11:40:38 -0600

    Creating session model and 1st form log in with validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,14 @@ class UsersController < ApplicationController
   def index
     @user = User.new
   end
-  
-  def show
-      
-  end
 
-  def new
-      
-  end
+  def show; end
 
-  def create
-      
-  end
+  def new; end
 
-  def edit
-      
-  end
-  
-  def update
-      
-  end
+  def create; end
+
+  def edit; end
+
+  def update; end
 end
