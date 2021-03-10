59cfa45df8c3095919b5e4f7b588010211123f2b
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-10 13:33:09 -0600

    Add creating user form and validation to don't repeat names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,18 @@ class UsersController < ApplicationController
 
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(username: params[:user][:username])
+    if @user.save
+      session[:user_id] = @user.id
+      session[:username] = @user.username
+      flash[:success] = "User created, welcome #{@user.username}"
+      redirect_to user_path(@user.id)
+    else
+      flash[:danger] = "User was not created, please try another name :c"
+      redirect_to new_user_url
+    end
+  end
 
   def edit; end
 
