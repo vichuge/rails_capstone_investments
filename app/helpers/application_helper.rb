8c354914113c0e42f8aace32716e135540cee0fb
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-11 17:52:14 -0600

    Adding name in inversions list to group

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def take_icon(id)
     Inversion.find(id).groups.map(&:icon)[0].nil? ? 'question' : Inversion.find(id).groups.map(&:icon)[0]
   end
+
+  def take_username(id)
+    User.find(id).username
+  end
 end
