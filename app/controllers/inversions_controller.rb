9053e6b13401b6b64edc92fcd00dbc85453b024d
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-11 11:45:58 -0600

    Change float to decimal in database and listing inversions without icons

--- app/controllers/inversions_controller.rb
+++ app/controllers/inversions_controller.rb
@@ -1,8 +1,8 @@
 class InversionsController < ApplicationController
   include SessionsHelper
   def index
-    @transactions = Inversion.where(author_id: current_user)
-    @total = @transactions.sum('amount')
+    @inversions = Inversion.where(author_id: current_user)
+    @total = @inversions.sum('amount')
   end
 
   def external
@@ -12,9 +12,26 @@ class InversionsController < ApplicationController
 
   def new
     @groups = Group.all.map { |g| [g.name, g.id] }
+    @groups.unshift(["select and option...", 0])
   end
 
-  def create; end
+  def create
+    @inversion = Inversion.new(name: params[:investment][:name], amount: params[:investment][:amount])
+    @inversion.author_id = session[:user_id]
+    
+    if @inversion.save
+      @group = params[:investment][:group]
+      if @group != 0
+        @relation = GroupInversion.new(group_id: @group, inversion_id: @inversion.id)
+        @relation.save
+      end
+      flash[:success] = 'Inversion created succesfully!'
+      redirect_to inversions_path
+    else
+      flash[:danger] = 'Inversion was not created, please try again :c'
+      redirect_to inversions_path
+    end
+  end
 
   def edit; end
 
