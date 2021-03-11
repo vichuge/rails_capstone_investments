69def50e1fe1af26f692cbd9a0f03b0746e4c908
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-11 14:52:24 -0600

    List all inversions and group in alphabetical order

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :group_inversions
   has_many :inversions, :through => :group_inversions
+
+  scope :alphabetical_order, -> {order(name: :asc)}
 end
