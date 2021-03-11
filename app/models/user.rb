29048b01a87acb794d5662dc22e4b80ecc033bbf
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-11 13:50:36 -0600

    Solvine ER with association table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates_uniqueness_of :username
 
-  has_many :transactions
+  has_many :inversions, dependent: :destroy
   has_many :groups
 end
